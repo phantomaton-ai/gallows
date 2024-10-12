d24e1fcb083aa62d5db9f72e4387c49b124f2606
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-12 16:52:39 -0700

    Test examples

--- gallows.test.js
+++ gallows.test.js
@@ -3,7 +3,7 @@ import gallows from './gallows.js';
 
 describe('Gallows', () => {
   describe('echo', () => {
-    it('should echo the provided body', () => {
+    it('echoes the provided body', () => {
       const result = gallows([
         {
           name: 'echo',
@@ -19,7 +19,7 @@ describe('Gallows', () => {
       expect(result).to.equal('Hello, Gallows!');
     });
 
-    it('should support command overloading', () => {
+    it('supports command overloading', () => {
       const result = gallows([
         {
           name: 'echo',
@@ -44,5 +44,36 @@ describe('Gallows', () => {
       ]).execute('echo', { message: 'Hello, world!' });
       expect(result).to.equal('Echo: Hello, world!');
     });
+  });
+
+  describe('examples', () => {
+    it('provides example directives', () => {
+      const result = gallows([
+        {
+          name: 'echo',
+          validate: (attributes, body) => body !== undefined,
+          execute: (attributes, body) => body,
+          example: {
+            attributes: {},
+            body: 'Hello, Gallows!'
+          },
+          description: 'Echoes the provided message'
+        },
+        {
+          name: 'echo',
+          validate: (attributes, body) => attributes.message !== undefined,
+          execute: (attributes, body) => `Echo: ${attributes.message}`,
+          example: {
+            attributes: { message: 'Hello, world!' },
+            body: undefined
+          },
+          description: 'Echoes the provided message'
+        }
+      ]).examples();
+      expect(result).to.deep.equal([
+        { action: 'echo', attributes: {}, body: 'Hello, Gallows!' },
+        { action: 'echo', attributes: { message: 'Hello, world!' }, body: undefined }
+      ]);      
+    })
   });
 });
