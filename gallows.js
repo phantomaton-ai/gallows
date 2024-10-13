beef256eda092bd562267f8051cd6069353fb12e
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-12 17:10:07 -0700

    coverage

--- gallows.js
+++ gallows.js
@@ -3,11 +3,7 @@ export default function gallows(commands) {
     examples: () => commands.map(cmd => ({ action: cmd.name, ...cmd.example })),
     execute: (action, attributes, body) => {
       const command = commands.find(cmd => cmd.name === action && cmd.validate(attributes, body));
-      if (command) {
-        return command.execute(attributes, body);
-      } else {
-        throw new Error(`Command '${action}' not found or invalid.`);
-      }
+      return command && command.execute(attributes, body);
     }
   };
 }
