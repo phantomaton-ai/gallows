9ab2c5e0663ebc07e618e7ff2d95d96ce9e8ebc0
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-12 17:17:20 -0700

    ⛳️

--- gallows.js
+++ gallows.js
@@ -1,9 +1,8 @@
 export default function gallows(commands) {
   return {
     examples: () => commands.map(cmd => ({ action: cmd.name, ...cmd.example })),
-    execute: (action, attributes, body) => {
-      const command = commands.find(cmd => cmd.name === action && cmd.validate(attributes, body));
-      return command && command.execute(attributes, body);
-    }
+    execute: (action, attributes, body) => commands.find(
+      cmd => cmd.name === action && cmd.validate(attributes, body)
+    )?.execute(attributes, body)
   };
 }
